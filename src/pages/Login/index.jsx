25003033eb4aa1f162504f391d082e93fe7039cc
Italo Russan <italo.russan@csktech.com.br>
2020-08-13 21:28:17 -0300

    fix: ajustes do form de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,7 +4,7 @@ import logoIoasys from '../../images/logo.png';
 export default function Login() {
     return(
         <section className="min-vh-100 d-flex align-items-center py-3">
-            <div className="login-box col-12 col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+            <div className="login-box col-10 col-md-6 col-lg-2 offset-1 offset-md-3 offset-lg-5">
                 <figure className="logo-login mx-auto">
                     <img src={logoIoasys} alt="IOASYS"/>
                 </figure>
@@ -12,10 +12,10 @@ export default function Login() {
                     <h1 className="text-center">BEM-VINDO AO<br/>EMPRESAS</h1>
                     <p className="text-center">Lorem ipsum dolor sit amet, consectetur<br/> adipisicing elit. Eos, ex?</p>
                 </div>
-                <form className="login-form d-flex flex-column">
-                    <input id="user_email" type="email"/>
-                    <input id="user_pass" type="password"/>
-                    <button type="button">ENTRAR</button>
+                <form className="login-form d-flex flex-column" autoComplete="off">
+                    <input className="error" id="user_email" type="email" placeholder="E-mail"/>
+                    <input className="error" id="user_pass" type="password" placeholder="Senha"/>
+                    <button className="btn-default mx-auto" type="button">ENTRAR</button>
                 </form>
             </div>
         </section>
