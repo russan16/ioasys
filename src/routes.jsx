3383a07090cae96f5cb0f76ef7e6d4f10b61c066
Italo Russan <italo.russan@csktech.com.br>
2020-08-14 13:16:39 -0300

    add: auth response

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 // PAGES
 import Login from './pages/Login';
+import Home from './pages/Home';
 
-const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Login}/>
-            </Switch>
-        </BrowserRouter>
-    )
-}
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route
+        {...rest}
+        render={props => isAuthenticated() ? (
+            <Component {...props}/>
+        ) : (
+            <Redirect to={{pathname: "/", state: {from: props.location}}}/>
+        )}
+    />
+);
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Login}/>
+            <PrivateRoute path="/home" component={Home}/>
+        </Switch>
+    </BrowserRouter>
+)
 export default Routes;
